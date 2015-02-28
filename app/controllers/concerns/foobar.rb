19d126a2c2f4513fb329e9f79793fd66e8af09cc
Tanya Krishnakumar <tanyakris@gmail.com>
2015-02-27 23:49:24 -0800

    Hi

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,2 +1,12 @@
+#Write a class function in Foobar.rb called baz that takes in an array of integers as strings, 
+#changes each string into an integer, adds two to each number, keeps the even numbers only, takes out any duplicates, 
+#rejects the resulting numbers greater than ten, and returns the sum of the resulting array. You should be able to write 
+#this with one line of code using method chaining!
+
+
 class Foobar
+	def self.baz(array)
+		array.uniq.map{|ele| ele.to_i}.map{|ele| ele += 2}.select{|ele| ele.even?}.select{|ele| ele <= 10}.inject{|sum,n| sum+n}
+
+	end
 end
---
+++ app/controllers/concerns/person.rb
@@ -0,0 +1,19 @@
+class Person
+	def initialize(name,age)
+		@name = name
+		@age = age
+		@nickname = name[0,3]
+	end
+
+	def introduce
+		"My name is " << @name << ". I am " << @age << " years old."
+	end
+
+	def birthyear
+		2015 - @age.to_i
+	end
+
+	def nickname
+		@nickname
+	end
+end
